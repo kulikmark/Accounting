49e57022b3ed38304fc74458ee046010e36a61ec
Mark Kulik <kulik_mark@mail.ru>
2024-04-13 12:15:28 +0300

    Initial Commit

---
+++ Accounting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Accounting
+//
+//  Created by Марк Кулик on 13.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
